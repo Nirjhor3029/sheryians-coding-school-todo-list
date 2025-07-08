fdf436f2bb0d11b0770f2593fcb6428978b5bf8a
Nirjhor3029 <sazzad3029@gmail.com>
2025-07-08 23:23:17 +0600

    Design a form for todo list

--- app/page.js
+++ app/page.js
@@ -1,8 +1,42 @@
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold ">Welcome to Next.js!</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold ">ToDo List</h1>
+      <form className="w-full max-w-sm mt-4">
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="task">
+            Task
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="task"
+            type="text"
+            placeholder="Enter task"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="description">
+            Description
+          </label>
+          <textarea
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="description"
+            placeholder="Enter description"
+            rows="4"
+          ></textarea>
+        </div>
+
+        <div className="flex items-center justify-between">
+          <button
+            className=" cursor-pointer bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Add Task
+          </button>
+        </div>
+      </form>
+
     </div>
   );
 }
